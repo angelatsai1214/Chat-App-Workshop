4b11db645b5c2b1a39a9452e5c8e58fb4d373b57
Cho Jung (Angela) Tsai <64194102+angelatsai1214@users.noreply.github.com>
2024-11-16 01:13:13 -0800

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
-
-
+// DO NOT CHANGE ANYTHING ABOVE HERE
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // TODO: copy paste your firebaseConfig to here!!!
 
 
+
+// DO NOT CHANGE ANYTHING BELOW HERE
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
